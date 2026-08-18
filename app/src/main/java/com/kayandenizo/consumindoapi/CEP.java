9d59e899e42861358ffdba58c214aa6e7879c2ce
Android Studio <Android@uirapuru.br>
2026-08-18 10:36:59 -0300

    Feat: adiciona classe CEP

---
+++ app/src/main/java/com/kayandenizo/consumindoapi/CEP.java
@@ -0,0 +1,14 @@
+package com.kayandenizo.consumindoapi;
+
+public class CEP {
+    private String cep;
+
+    private String logradouro;
+
+    private String complemento;
+
+    private String bairro;
+
+    private String uf;
+
+}
